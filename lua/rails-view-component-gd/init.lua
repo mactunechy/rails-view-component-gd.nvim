98017f8e03be93c2b27c45bbfa962f48e866d959
Dellan Muchengapadare <mactunechy@gmail.com>
2024-04-30 22:30:05 +0200

    add template and class toggle

--- lua/rails-view-component-gd/init.lua
+++ lua/rails-view-component-gd/init.lua
@@ -1,7 +1,8 @@
 
+local COMPONENTS_FOLDER = 'app/components/'
 
-local get_file_path = function ()
-   local line = vim.api.nvim_get_current_line()
+local get_file_path = function()
+  local line = vim.api.nvim_get_current_line()
 
   local namespace = line:match("(%w+::%w+)")
 
@@ -13,14 +14,14 @@ local get_file_path = function ()
     return print("Not a view component")
   end
 
-  local file_path = namespace:gsub("::", "/"):gsub("(%w)(%u)", function (a,b)
+  local file_path = namespace:gsub("::", "/"):gsub("(%w)(%u)", function(a, b)
     return a .. "_" .. b
   end):lower()
 
   return file_path
 end
 
-local open_file = function (path)
+local open_file = function(path)
   if vim.loop.fs_stat(path) then
     vim.cmd("edit " .. path)
   end
@@ -29,22 +30,54 @@ end
 
 -- Public API ---
 
-local M = {}
+local M         = {}
 
-M.open_class = function ()
-  local  file_path = get_file_path()
-  local relative_path = "app/components/" .. file_path .. '.rb'
+M.open_class    = function()
+  local file_path = get_file_path()
+  local relative_path = COMPONENTS_FOLDER .. file_path .. '.rb'
 
   open_file(relative_path)
 end
 
-M.open_template = function ()
-  local file_extension = vim.fn.expand("%:e")
-  local  file_path = get_file_path()
-  local relative_path = "app/components/" .. file_path .. '.html.' .. file_extension
+M.open_spec     = function()
+  local file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~:."):gsub("%..*", "")
+  local destination = file_path:gsub("app", "spec") .. "_spec.rb"
 
-  open_file(relative_path)
+  if not vim.loop.fs_stat(destination) then
+    return print("ViewComponent spec does not exist")
+  end
+
+  open_file(destination)
+end
+
+M.open_template = function()
+  local file_extension = vim.fn.expand("%:e")
+  local file_path = get_file_path()
+  local destination = COMPONENTS_FOLDER .. file_path .. '.html.' .. file_extension
+
+  open_file(destination)
+end
+
+M.toggle        = function()
+  local file_extension = vim.fn.expand("%:e")
+  local file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~:."):gsub("%..*", "")
+
+  local desitnation_file_paths = {
+    slim = function() return file_path .. ".rb" end,
+    erb  = function() return file_path .. ".rb" end,
+    rb   = function()
+      local relative_path = file_path .. ".html.slim"
+
+      if not vim.loop.fs_stat(relative_path) then
+        relative_path = file_path .. ".html.erb"
+      end
+
+      return relative_path
+    end
+  }
+
+  local destination = desitnation_file_paths[file_extension]()
+  open_file(destination)
 end
 
 return M
-
